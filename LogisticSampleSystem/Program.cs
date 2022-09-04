80b42860012cf065a36fdb6a7d5d9d50f1840884
JGMelon22 <jgvasconcellos22@gmail.com>
2022-09-04 14:25:13 -0300

    Updating and Deleting

--- LogisticSampleSystem/Program.cs
+++ LogisticSampleSystem/Program.cs
@@ -3,6 +3,15 @@
 Console.Clear();
 
 Console.WriteLine("=== Mock Logistic System ===");
+
+for (int i = 0; i < 50; i++)
+{
+    LogisticsService.PostMockInfo();
+}
 // LogisticsService.PostMockInfo(); // Post Data
 
-LogisticsService.GetData();
+LogisticsService.GetDataById(40);
+LogisticsService.UpdateMethod(39);
+// LogisticsService.UpdateMethod(8);
+// LogisticsService.DeleteMethod(40);
+// LogisticsService.GetData();
--- LogisticSampleSystem/Services/LogisticsService.cs
+++ LogisticSampleSystem/Services/LogisticsService.cs
@@ -42,7 +42,7 @@ public static class LogisticsService
                      .Include(y => y.Product)
                      .OrderByDescending(z => z.Id)
                      .ToList();
-        
+
         foreach (var item in orders)
         {
             Console.WriteLine($"Customer: {item.Customer.CustomerName}");
@@ -50,5 +50,51 @@ public static class LogisticsService
             Console.WriteLine($"Product Id: {item.Product.Id}");
             Console.WriteLine($"Product Quantity: {item.Quantity}");
         }
+    }
+
+    // By Id
+    public static void GetDataById(int id)
+    {
+        using var context = new LogisticsDataContext();
+        var orders = context.Orders
+                     .Include(x => x.Customer)
+                     .Include(y => y.Product)
+                     .OrderByDescending(z => z.Id)
+                     .ToList();
+
+        foreach (var item in orders)
+        {
+            Console.WriteLine($"Customer: {item.Customer.CustomerName}");
+            Console.WriteLine($"Customer Country: {item.Customer.Country}");
+            Console.WriteLine($"Product Id: {item.Product.Id}");
+            Console.WriteLine($"Product Quantity: {item.Quantity}");
+        }
+    }
+
+
+    public static void UpdateMethod(int id)
+    {
+        using var context = new LogisticsDataContext();
+        var order = context.Orders
+            .Include(x => x.Customer)
+            .Include(y => y.Product)
+            .OrderByDescending(z => z.Id)
+            .FirstOrDefault();
+
+        order.Customer.CustomerName = "Customer recently updated";
+        context.Orders.Update(order);
+        context.SaveChanges();
+    }
+
+    public static void DeleteMethod(int id)
+    {
+        using var context = new LogisticsDataContext();
+        var customerToRemove = context.Customers.FirstOrDefault(x => x.Id == id);
+
+        context.Remove(customerToRemove);
+        context.SaveChanges();
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"Customer {id} deleted!");
+        Console.ResetColor();
     }
 }
